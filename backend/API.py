6a6f6877d561642ceac9a981a80da1ed993b3bdb
Thang Nguyen <nguyenn.thang@yahoo.com>
2023-03-12 18:23:22 -0500

    Backend updated, registration page submits data successfully

--- backend/API.py
+++ backend/API.py
@@ -5,18 +5,20 @@ import mysql.connector
 from mysql.connector import Error
 from sql import create_connection, execute_query, execute_read_query
 from datetime import datetime
+from flask_cors import CORS
 
 conn = create_connection('172.26.54.22', 'ADMIN', 'password123!', 'llc_database') # connection to database
 
 # setup application
 app = flask.Flask(__name__)
+CORS(app)
 app.config["DEBUG"] = True
 
 ##### returns all the volunteers in the volunteers table ######
 @app.route('/', methods = ['GET']) # http://127.0.0.1:5000/
 def home():
     
-    query = "SELECT volunteers.first_name, volunteers.last_name, volunteers.phone FROM volunteers" 
+    query = "SELECT volunteers.first_name, volunteers.last_name, volunteers.phone, volunteers.email, volunteers.emergency_contact_fname, emergency_contact_lname, emergency_contact_phone FROM volunteers" 
     rows = execute_read_query(conn,query)
     return jsonify(rows)
 
@@ -25,12 +27,16 @@ def add_volunteer():
     request_data = request.get_json() # stores json input into variables
     first_name = request_data['first_name']
     last_name = request_data['last_name']
-    phone_number = request_data['phone_number']
+    phone = request_data['phone']
+    email = request_data['email']
+    emergency_contact_fname = request_data['emergency_contact_fname']
+    emergency_contact_lname = request_data['emergency_contact_lname']
+    emergency_contact_phone = request_data['emergency_contact_phone']
 
     
     ### query for inserting data ###
-    query = "INSERT INTO volunteers (first_name, last_name, phone) VALUES ('%s', '%s', '%s')" \
-        % (first_name, last_name, phone_number) # inserts new entry in volunteers table
+    query = "INSERT INTO volunteers (first_name, last_name, phone, email, emergency_contact_fname, emergency_contact_lname, emergency_contact_phone) VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s')" \
+        % (first_name, last_name, phone, email, emergency_contact_fname, emergency_contact_lname, emergency_contact_phone) # inserts new entry in volunteers table
    
     execute_query(conn, query)
 
--- frontend/src/views/Register.vue
+++ frontend/src/views/Register.vue
@@ -5,26 +5,26 @@
     </div>
     <div class="container"> 
         <h3> Volunteer</h3>
-        <form>
+        <form @submit.prevent="submitForm">
             <div>
                 <div class="row">
                     <div class="col"> 
                         <label for="exampleFormControlInput1" class="form-label">First Name</label>
-                        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="John">
+                        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="John" v-model="volunteers.first_name">
                     </div>
                     <div class="col"> 
                         <label for="exampleFormControlInput1" class="form-label">Last Name</label>
-                        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Smith">
+                        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Smith" v-model="volunteers.last_name">
                     </div>
                 </div>
                 <div class="row">
                     <div class="col"> 
                         <label for="exampleFormControlInput1" class="form-label">Phone Number</label>
-                        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="1234567890">
+                        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="1234567890" v-model="volunteers.phone">
                     </div>
                     <div class="col"> 
                         <label for="exampleFormControlInput1" class="form-label">Email</label>
-                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" v-model="volunteers.email">
                     </div>
                 </div>
             </div>
@@ -34,27 +34,23 @@
                 <div class="row">
                     <div class="col"> 
                         <label for="exampleFormControlInput1" class="form-label">First Name</label>
-                        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="John">
+                        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="John" v-model="volunteers.emergency_contact_fname">
                     </div>
                     <div class="col"> 
                         <label for="exampleFormControlInput1" class="form-label">Last Name</label>
-                        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Smith">
+                        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Smith" v-model="volunteers.emergency_contact_lname">
                     </div>
                 </div>
                 <div class="row">
                     <div class="col"> 
                         <label for="exampleFormControlInput1" class="form-label">Phone Number</label>
-                        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="1234567890">
-                    </div>
-                    <div class="col"> 
-                        <label for="exampleFormControlInput1" class="form-label">Email</label>
-                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+                        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="1234567890" v-model="volunteers.emergency_contact_phone">
                     </div>
                 </div>
                 
             </div>
             <div style="text-align:right; margin-top: 2rem">
-                <button @click="handleEventUpdate" type="submit" class="btn btn-primary" >Register</button>
+                <button type="submit" class="btn btn-primary" >Register</button>
           </div>
         </form>
     </div>
@@ -62,13 +58,40 @@
 </template>
 
 <script>
+import axios from "axios";
 
 export default {
     name: 'Register',
     data() {
         return {
-            msg : "Volunteer Registration Form"
+            msg : "Volunteer Registration Form",
+            volunteers: {
+                first_name: '',
+                last_name: '',
+                phone:  '',
+                email: '',
+                emergency_contact_fname: '',
+                emergency_contact_lname: '',
+                emergency_contact_phone: '',
+
+
+            },
+        };
+
+    },
+    methods: {
+        submitForm() {
+            axios
+                .post('http://127.0.0.1:5000/add_volunteer', this.volunteers)
+                .then(() => {
+                    this.volunteers = {}
+                })
+                .catch((error) => {
+                    console.log(error);
+          });
+
         }
+
 
     }
 }
