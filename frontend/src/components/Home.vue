824c430e28750b2cb1d58e0def21af91081325b9
erikssonphil <paeriksson@uh.edu>
2023-03-13 21:55:58 -0500

    apply bootstrap styles

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,73 +1,97 @@
 <template>
     <main>
-    <div class="container">
-        <div>
-            <h1 style="text-align: center; margin-top: 2rem; margin-bottom: 2rem"> {{ msg }}</h1>
+      <div class="container">
+        <div class="text-center mt-5">
+          <h1>{{ msg }}</h1>
         </div>
-        <form style="text-align: center;" @submit.prevent="handleSubmitForm">
-            <div style="text-align: center">
-                <h2>Enter Phone Number</h2>
+        <form class="mt-5" @submit.prevent="handleSubmitForm">
+          <div class="form-group text-center">
+            <h2>Enter Phone Number</h2>
+          </div>
+          <div class="form-group">
+            <input type="tel" class="form-control mx-auto" required style="max-width: 300px" @keydown="reviewKey" v-model="phoneNumber">
+            <div class="invalid-feedback" v-if="error">
+              Phone number must be 10 digits.
             </div>
-            <input type="tel" required style="width: 50%" @keydown="reviewKey" v-model="phoneNumber">
-            <p v-if="error" style="color: red">
-                Phone number must be 10 digits.
-            </p>
+          </div>
+          <div class="text-center">
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </div>
         </form>
-    </div>
+      </div>
     </main>
-</template>
-
-<script>
-
-export default {
+  </template>
+  
+  <script>
+  export default {
     name: 'Home',
     data() {
-        return {
-            msg : "Welcome to The Living Legacy Center",
-            phoneNumber: null,
-            acceptedKeys: [
-                "0",
-                "1",
-                "2",
-                "3",
-                "4",
-                "5",
-                "6",
-                "7",
-                "8",
-                "9",
-                "Backspace",
-                "Delete",
-                "ArrowLeft",
-                "ArrowRight",
-                "Enter"
-            ],
-            error: false
-        }
+      return {
+        msg : "Welcome to The Living Legacy Center",
+        phoneNumber: null,
+        acceptedKeys: [
+          "0",
+          "1",
+          "2",
+          "3",
+          "4",
+          "5",
+          "6",
+          "7",
+          "8",
+          "9",
+          "Backspace",
+          "Delete",
+          "ArrowLeft",
+          "ArrowRight",
+          "Enter"
+        ],
+        error: false
+      }
     },
     methods: {
-        handleSubmitForm() {
-            const phoneNumberRegex = /^\d{10}$/; // regular expression to match 10 digits
-            if (phoneNumberRegex.test(this.phoneNumber)) {
-                this.error = false
-                alert('Submitted');
-            } else {
-                this.error = true
-            }
-        },
-        reviewKey(e) {
-            console.log(e)
-            if (!this.acceptedKeys.includes(e.key)) {
-                e.preventDefault()
-            }
+      handleSubmitForm() {
+        const phoneNumberRegex = /^\d{10}$/; // regular expression to match 10 digits
+        if (phoneNumberRegex.test(this.phoneNumber)) {
+          this.error = false
+          alert('Submitted');
+        } else {
+          this.error = true
         }
+      },
+      reviewKey(e) {
+        if (!this.acceptedKeys.includes(e.key)) {
+          e.preventDefault()
+        }
+      }
     }
-}
-</script>
-
-<style>
-.container {
-  margin: auto;
-
-}
-</style>
+  }
+  </script>
+  
+  <style scoped>
+  .container {
+    max-width: 500px;
+    margin: auto;
+  }
+  
+  .form-control:focus {
+    box-shadow: none;
+  }
+  
+  .invalid-feedback {
+    display: block;
+    margin-top: 0.5rem;
+    color: #dc3545;
+  }
+  
+  .btn-primary {
+    background-color: #3f51b5;
+    border-color: #3f51b5;
+  }
+  
+  .btn-primary:hover {
+    background-color: #303f9f;
+    border-color: #303f9f;
+  }
+  </style>
+  
