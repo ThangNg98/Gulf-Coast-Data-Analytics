2f0bd3b9fd3aea4135150b41e1adacc7ceecd195
erikssonphil <paeriksson@uh.edu>
2023-03-14 04:08:32 -0500

    added comments to admin_login

--- frontend/src/Views/AdminLogin.vue
+++ frontend/src/Views/AdminLogin.vue
@@ -1,16 +1,21 @@
 <template>
   <div class="container">
+    <!--Header-->
     <h1 class="text-center my-5">Administrator Login</h1>
+    <!--Form for Admin Login-->
     <form @submit.prevent="handleLogin">
+      <!--User Name input field-->
       <div class="form-group">
         <label for="username">User Name</label>
         <input type="text" class="form-control" id="username" v-model="username">
       </div>
+      <!--Password Input Field-->
       <div class="form-group">
         <label for="password">Password</label>
         <input type="password" class="form-control" id="password" v-model="password">
       </div>
       <br>
+      <!--Submit Button-->
       <div class="text-center">
           <button type="submit" class="btn btn-primary">Submit</button>
       </div>
@@ -19,26 +24,29 @@
 </template>
 
 <script>
+//Import "useAdminLoginStore" to save logged in status as session
 import { useAdminLoginStore } from '@/stores/AdminLoginStore'
 
 export default {
   data() {
     return {
+      //variable to hold username
       username: '',
+      //variable to hold password - need to store as hash
       password: ''
     }
   },
   computed: {
+    // code to check if admin is logged in - used to redirect user later to admin dashboard
     isLoggedIn() {
       useAdminLoginStore().isLoggedIn
     }
   },
   methods: {
+    //method to log admin in
     handleLogin() {
-      // perform login logic
-      console.log('useAdminLoginStore().isLoggedIn', useAdminLoginStore().isLoggedIn)
+      //calls the "login()" action from the store file, which sets isLoggedIn state to true
       useAdminLoginStore().login()
-      console.log('useAdminLoginStore().isLoggedIn', useAdminLoginStore().isLoggedIn)
     }
   }
 }
