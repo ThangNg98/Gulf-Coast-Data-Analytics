d867ff4f219c5bc475c3113ecd87948b0c04b1a9
Mizba-Anjum <89572205+Mizba-Anjum@users.noreply.github.com>
2023-03-19 18:48:23 -0500

    add /profile back in

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -16,6 +16,7 @@ const router = createRouter({
       component: () => import('../views/Register.vue')
     },
     {
+      path: '/profile',
       name: 'Profile',
       props: true,
       component: () => import('../views/Profile.vue'),
