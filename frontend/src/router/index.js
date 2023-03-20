85a9cec1b8f6001d42474c5f01549273afbfa678
Thang Nguyen <nguyenn.thang@yahoo.com>
2023-03-20 12:52:56 -0500

    Volunteers page complete

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -15,6 +15,18 @@ const router = createRouter({
       props: true,
       component: () => import('../views/Register.vue')
     },
+    {
+      path: '/volunteers',
+      name: 'Volunteers',
+      props: true,
+      component: () => import('../components/Volunteers.vue')
+    },
+    {
+      path: '/update_volunteer',
+      name: 'VolunteersUpdate',
+      props: true,
+      component: () => import('../components/VolunteersUpdate.vue')
+    },
   ]
 })
 
