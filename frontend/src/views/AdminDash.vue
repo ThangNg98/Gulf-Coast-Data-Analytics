5bbc48017acab2eb04bf9a8897dbe1158e2bdeb6
Mizba-Anjum <89572205+Mizba-Anjum@users.noreply.github.com>
2023-03-26 13:21:17 -0500

    menu

--- frontend/src/views/AdminDash.vue
+++ frontend/src/views/AdminDash.vue
@@ -1,14 +1,41 @@
 <!--'/admindash'-->
 <template>
-    <main class="text-center">
-        <div class="d-inline-block">
-            <nav class="in-page-nav-bar nav  border border-dark justify-content-center" style="margin-top: 2rem;">
-                <router-link class="nav-link border-end border-dark" to="/admin/dash">Dashboard</router-link>
-                <router-link class="nav-link border-end border-dark" to="/admin/events">Events</router-link>
-                <router-link class="nav-link border-end border-dark" to="/admin/orgs">Organizations</router-link>
-                <router-link class="nav-link border-end border-dark" to="/admin/volunteers">Volunteers</router-link>
-                <a class="nav-link">Sessions</a>
-            </nav>
+    <main class="">
+        <div class="d-inline-block text-start ms-3">
+            <nav id="contentNav" class="navbar navbar-expand-lg navbar-light navbar-brand" style="margin-top:1rem; margin-bottom:1rem">
+            <button
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#contentNavbar"
+              class="navbar-toggler"
+              aria-controls="contentNavbar"
+              aria-expanded="false"
+              aria-label="Toggle navigation" style="margin-top:1rem;">
+              
+              <span class="navbar-toggler-icon"></span> Menu
+            </button>
+              <div class="collapse navbar-collapse" id="contentNavbar">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0 flex-wrap">
+                  <li class="nav-item">
+                    <router-link class="nav-link text-primary" to="/admin/dash">Dashboard</router-link>
+                  </li>
+                  <li class="nav-item">
+                    <router-link class="nav-link text-primary" to="/admin/events">Events</router-link>
+                  </li>
+                  <li class="nav-item">
+                    <router-link class="nav-link text-primary" to="/admin/orgs">Organizations</router-link>
+                  </li>
+                  <li class="nav-item">
+                    <router-link class="nav-link text-primary" to="/admin/volunteers">Volunteers</router-link>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link text-primary">Sessions</a>
+                  </li>
+                  
+                </ul>
+            </div>
+      </nav>
+
         </div>
         <div>
             <router-view></router-view>
@@ -16,7 +43,35 @@
     </main>
 </template>
 <style>
-    .in-page-nav-bar .nav-link.router-link-exact-active{
+    #contentNavbar .nav-link.router-link-exact-active{
         background-color: #eee;
+    }
+     /* Extra Small Devices, Phones */ 
+    @media only screen and (min-width : 480px) {
+
+    }
+
+    /* Small Devices, Tablets */
+    @media only screen and (min-width : 768px) {
+
+    }
+
+    /* Medium Devices, Desktops */
+    @media only screen and (min-width : 992px) {
+        main {
+            text-align: center;
+        }
+        #contentNavbar .nav-item {
+            border: 1px solid black;
+            border-right: none;
+        }
+        #contentNavbar .nav-item:last-child {
+            border: 1px solid black;
+        }
+    }
+
+    /* Large Devices, Wide Screens */
+    @media only screen and (min-width : 1200px) {
+
     }
 </style>
