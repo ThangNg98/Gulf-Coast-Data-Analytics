9fb1c6b12be0a90e60d15abbd780eb29b4518f50
Mizba-Anjum <89572205+Mizba-Anjum@users.noreply.github.com>
2023-03-30 20:02:58 -0500

    8 hour automation complete

--- backend/API.py
+++ backend/API.py
@@ -26,14 +26,14 @@ def create_session():
     session_status_id = request_data['session_status_id']
     volunteer_id = request_data['volunteer_id']
     
-    event_name = "event " + str(time_in)
+    event_name = "event" + str(time_in).replace(":", "")
     
     query = "INSERT INTO session (time_in, session_date, session_comment, org_id, event_id, session_status_id, volunteer_id) VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s');" \
         % (time_in, session_date, session_comment, org_id, event_id, session_status_id, volunteer_id)
     execute_query(conn,query)
     
-    query_auto_logout = "CREATE EVENT '%s' ON SCHEDULE AT CURRENT_TIMESTAMP + INTERVAL 1 SECOND DO UPDATE session SET time_out = NOW() where time_in = '%s';" \
-        % (event_name, time_in)
+    query_auto_logout = "CREATE EVENT " + event_name + " ON SCHEDULE AT CURRENT_TIMESTAMP + INTERVAL 8 HOUR DO UPDATE session SET time_out = NOW() where time_in = '%s';" \
+        % (time_in)
     execute_query(conn,query_auto_logout)
     
     return "Add session request successful"
