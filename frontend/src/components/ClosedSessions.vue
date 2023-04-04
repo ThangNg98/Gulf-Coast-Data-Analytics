4882a304b20c2b757d9fd48f87aead492431a9e2
Thang Nguyen <nguyenn.thang@yahoo.com>
2023-04-03 19:03:20 -0500

    Changed active to open

--- frontend/src/components/ClosedSessions.vue
+++ frontend/src/components/ClosedSessions.vue
@@ -50,7 +50,7 @@ export default {
     name: 'ClosedSessions',
     data() {
         return {
-            msg : "Active",
+            msg : "Open",
             msg2 : "Closed",
             sessions:[],
             hoverId: null,
