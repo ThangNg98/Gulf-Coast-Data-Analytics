d642799ebf06268430c0caa33ec0c1b87aaa5b4b
erikssonphil <paeriksson@uh.edu>
2023-04-04 20:18:13 -0500

    completed updateorg validation and alerts

---
+++ frontend/src/components/DeleteModal.vue
@@ -0,0 +1,118 @@
+<template>
+
+    <Transition name="bounce">
+      <div class="alert-modal alert alert-danger alert-dismissible fade show" role="alert" v-if="isVisible">
+        <h4 class="alert-heading">{{ title }}</h4>
+        <p>{{ message }}</p>
+        <button type="button" class="btn-close" aria-label="Close" @click="closeModal"></button>
+      </div>
+    </Transition>
+  
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      title: {
+        type: String,
+        default: "Alert"
+      },
+      message: {
+        type: String,
+        default: ""
+      }
+    },
+    data() {
+      return {
+        isVisible: true
+      }
+    },
+    mounted() {
+      this.startTimer();
+    },
+    methods: {
+      startTimer() {
+        setTimeout(() => {
+          this.isVisible = false;
+          this.$emit("close");
+        }, 5000); // close after 5 seconds
+      },
+      closeModal() {
+        this.isVisible = false;
+        this.$emit("close");
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+.alert-modal {
+  position: fixed;
+  top: 20rem;
+  right: 1rem;
+  transform: none;
+  padding: 1rem 1.25rem;
+  width: 400px;
+  max-width: 90%;
+  box-shadow: 0 5px 15px rgba(0, 0, 0, 0.5);
+}
+
+.alert-modal::after {
+  content: '';
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 4px;
+  background-color: #8B0000;
+  animation: decrease-width 5s linear forwards;
+}
+
+@keyframes decrease-width {
+  from {
+    width: 100%;
+  }
+  to {
+    width: 0;
+  }
+}
+
+.alert-modal h4 {
+  margin-bottom: 0.5rem;
+}
+
+.alert-modal p {
+  margin-bottom: 0;
+}
+
+.alert-modal-close-button {
+  border: none;
+  background: transparent;
+  font-size: 1.5rem;
+  cursor: pointer;
+}
+
+.bounce-enter-active {
+  transform: translate(-50%, -50%);
+  animation: bounce-in 0.5s;
+}
+
+.bounce-leave-active {
+  transform: translate(-50%, -50%);
+  animation: bounce-in 0.5s reverse;
+}
+
+@keyframes bounce-in {
+  0% {
+    transform: translate(0, 0) scale(0);
+  }
+  50% {
+    transform: translate(0, 0) scale(1.25);
+  }
+  100% {
+    transform: translate(0, 0) scale(1);
+  }
+}
+
+
+  </style>
--- frontend/src/components/Orgs.vue
+++ frontend/src/components/Orgs.vue
@@ -14,7 +14,7 @@
                     </thead>
                     <tbody>
                         <tr v-for="org in orgs"
-                        @click="editOrgs(org.org_id)">
+                        @click="editOrgs(org.org_id)" :key="org.org_id">
                             <td style="text-align:left">{{ org.org_name }}</td>
                             <td style="text-align:left">{{ org.address_line_1 }}</td>
                         </tr>
@@ -26,22 +26,77 @@
         </div>
 
     </div>
+
+    <Transition name="bounce">
+        <UpdateModal v-if="updateModal" @close="closeUpdateModal" :title="title" :message="message" />
+    </Transition>
+
+    <Transition name="bounce">
+        <DeleteModal v-if="deleteModal" @close="closeDeleteModal" :title="title" :message="message" />
+    </Transition>
+
     </main>
 </template>
 
 <script>
 import axios from "axios";
+import UpdateModal from './UpdateModal.vue'
+import DeleteModal from './DeleteModal.vue'
+
 export default {
     name: 'Orgs',
+    components: {
+        UpdateModal,
+        DeleteModal
+    },
     data() {
         return {
             msg : "List of Organizations",
-            orgs:[]
+            orgs:[],
+            successModal: false,
+            updateModal: false,
+            deleteModal: false,
+            title: '',
+            message: '',
+            isMounted: false
         };
     
     },
+    updated() {
+        if (!this.isMounted) {
+            console.log('pseudo mount')
+            const query = new URLSearchParams(this.$route.query);
+            if (query.get('success') === 'true') {
+                console.log('success is true')
+                this.successModal = true;
+                this.title = "Success!"
+                this.message = "Organization successfully created."
+            }
+            if (query.get('update') === 'true') {
+                console.log('update is true')
+                this.updateModal = true;
+                this.title = "Updated!"
+                this.message = "Organization successfully updated."
+            }
+            if (query.get('delete') === 'true') {
+                console.log('delete is true')
+                this.deleteModal = true;
+                this.title = "Deleted!"
+                this.message = "Organization successfully deleted."
+            }
+            this.isMounted = true
+        }
+    },
     methods: {
-        submitForm() {
+        closeUpdateModal() {
+            this.updateModal = false;
+            this.title = '';
+            this.message = '';
+        },
+        closeDeleteModal() {
+            this.deleteModal = false;
+            this.title = '';
+            this.message = '';
         },
         getOrgs() {
             axios.get('http://127.0.0.1:5000/read_orgs')
