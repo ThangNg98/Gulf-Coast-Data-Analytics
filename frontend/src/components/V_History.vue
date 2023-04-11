a32d20343848e028cb9358150ba6453f5995fc43
erikssonphil <paeriksson@uh.edu>
2023-04-11 01:59:54 -0500

    fixes

--- frontend/src/components/V_History.vue
+++ frontend/src/components/V_History.vue
@@ -1,26 +1,32 @@
 <template>
     <div v-if="isLoaded">
-        <div class="d-flex justify-content-center">
-            <div class="d-inline-flex flex-column w-75 text-start" style="min-width:300px">
-                <!--graph: named 'HoursPerYear' but data is sessions per month in the last six months-->
-                <div v-if="isLoaded">
-                <h3>Hours per Month in the Last Six Months</h3><HoursPerYear :listOfMonths="this.listOfMonths.reverse()" :listOfHours="this.listOfHours.reverse()"></HoursPerYear></div>
-                <br>
-                <!--table: session history from the last six months-->
-                <div><h3>Session History From the Last Six Months</h3><SessionsTable :sessions="this.sessions"></SessionsTable></div>
-            </div>
+      <div class="d-flex justify-content-center">
+        <div class="d-inline-flex flex-column w-75 text-start" style="min-width: 300px;">
+          <!--graph: named 'HoursPerYear' but data is sessions per month in the last six months-->
+          <div>
+            <h3>Hours per Month in the Last Six Months</h3>
+            <HoursPerYear :listOfMonths="this.listOfMonths" :listOfHours="this.listOfHours"></HoursPerYear>
+          </div>
+          <br>
+          <!--table: session history from the last six months-->
+          <div>
+            <h3>Session History From the Last Six Months</h3>
+            <SessionsTable :sessions="this.sessions"></SessionsTable>
+          </div>
         </div>
+      </div>
     </div>
-    
+  
     <div>
-        <LoadingModal v-if="!isLoaded"></LoadingModal>
+      <LoadingModal v-if="!isLoaded"></LoadingModal>
     </div>
-
+  
     <Transition name="bounce">
-        <UpdateModal v-if="updateModal" @close="closeUpdateModal" :title="title" :message="message" />
+      <UpdateModal v-if="updateModal" @close="closeUpdateModal" :title="title" :message="message" />
     </Transition>
+  
+  </template>
 
-</template>
 <script>
     import HoursPerYear from './V_HoursPerYearChart.vue';
     import SessionsTable from './V_SessionsTable.vue';
@@ -71,6 +77,7 @@
                 this.message = '';
             },
             async getInfo() {
+                console.log('getInfo')
                 try {
                     await this.getSessionHistory();
                     await this.getHoursHistory();
@@ -90,6 +97,7 @@
                 this.isLoaded = true;
             },
             async getSessionHistory() {
+                console.log('getSessionHistory')
                 try {
                     const response = await getSessionHistoryAPI(this.volunteer_id);
                     this.sessions = response.data;
@@ -98,6 +106,7 @@
                 }
             },
             async getHoursHistory() {
+                console.log('getHoursHistory')
                 try {
                     const response = await getHoursHistoryAPI(this.volunteer_id);
                     for (var i = 0; i < response.data.length; i++) {
@@ -105,6 +114,8 @@
                         this.listOfHours.push(JSON.parse(response.data[i].hours));
                         //months
                         this.listOfMonths.push(response.data[i].month);
+                        this.listOfMonths.reverse();
+                        this.listOfHours.reverse();
                     }
                 } catch (error) {
                     console.log(error)
@@ -114,3 +125,29 @@
         }
     }
 </script>
+
+<style scoped>
+body {
+  margin: 0;
+}
+
+.container1 {
+  max-height: 500px;
+  overflow: auto;
+  display:inline-block;
+  width: 100%;
+}
+
+.responsive-text {
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    font-size: 16px;
+}
+
+@media (max-width: 576px) {
+    .responsive-text {
+        font-size: 14px;
+    }
+}
+</style>
--- frontend/src/components/V_SessionsTable.vue
+++ frontend/src/components/V_SessionsTable.vue
@@ -1,52 +1,116 @@
 <template>
     <div class="container1 table-wrapper"> 
-    <div class="table-responsive-md">
-        <table class="table table-hover">
-            <thead>
-                <tr>
-                    <th scope="col">Date</th>
-                    <th scope="col">Event</th>
-                    <th scope="col">Organization</th>
-                    <th scope="col">Total Hours</th>
-                </tr>
-            </thead>
-            <tbody>
-                <!-- for each session, print event name, org name, hours, and formatted date-->
-                <tr v-for="session in this.sessions">
-                    <td>{{formatDate(session.dateval) }}</td>
-                    <td>{{session.eventName}}</td>
-                    <td>{{session.orgName}}</td>
-                    <td>{{session.hours}}</td>
-                </tr>
-            </tbody>
+      <div class="table-responsive-md">
+        <table class="table table-hover" style="width: 100%; table-layout: fixed;">
+          <colgroup>
+            <col style="width: 25%;">
+            <col style="width: 25%;">
+            <col style="width: 25%;">
+            <col style="width: 25%;">
+          </colgroup>
+          <thead class="theadsticky">
+            <tr>
+              <th scope="col" style="width: 25%;"><div class="responsive-text">Date</div></th>
+              <th scope="col" style="width: 25%;"><div class="responsive-text">Event</div></th>
+              <th scope="col" style="width: 25%;"><div class="responsive-text">Organization</div></th>
+              <th scope="col" style="width: 25%;"><div class="responsive-text">Total Hours</div></th>
+            </tr>
+          </thead>
+          <tbody>
+            <!-- for each session, print event name, org name, hours, and formatted date-->
+            <tr v-for="session in processedSessionsData">
+              <td style="width: 25%;"><div class="responsive-text">{{session.dateval}}</div></td>
+              <td style="width: 25%;"><div class="responsive-text">{{session.eventName}}</div></td>
+              <td style="width: 25%;"><div class="responsive-text">{{session.orgName}}</div></td>
+              <td style="width: 25%;"><div class="responsive-text">{{session.hours}}</div></td>
+            </tr>
+          </tbody>
         </table>
+      </div>
     </div>
-</div>
-</template>
+  </template>
+  
+
 <script>
-    export default {
-        props: {
-            sessions: Object
-        },
-        methods: {
-            formatDate(current) { //assuming Date method, returns formatted date string m/d/yyyy
-                var myDate = new Date(current);
-                
-                var output = (myDate.getMonth()+1) + "/" + (myDate.getDate()+1) + "/" +  myDate.getFullYear();
-                return output;
-                //return output
-            },
-            test() {
-                const text = 'test';
-                return text;
-            }
-        }
+export default {
+  props: {
+    sessions: Object
+  },
+  data() {
+    return {
+      processedSessionsData: [],
     }
+  },
+  computed: {
+    formattedSessionsData() {
+      return this.processedSessionsData.map(session => {
+        const formattedDate = this.formattedDate(session.dateval);
+        return {
+          ...session,
+          eventName: this.truncatedEventName(session.eventName),
+          orgName: this.truncatedOrgName(session.orgName),
+          hours: this.truncatedHours(session.hours),
+          dateval: formattedDate,
+        };
+      });
+    }
+  },
+  mounted() {
+    console.log('mounted Sessions Table')
+    this.processSessions();
+  },
+  methods: {
+        processSessions() {
+        this.processedSessionsData = this.sessions.map(session => {
+            const formattedDate = this.formattedDate(session.dateval);
+            const processedSession = {
+            ...session,
+            eventName: this.truncatedEventName(session.eventName),
+            orgName: this.truncatedOrgName(session.orgName),
+            hours: this.truncatedHours(session.hours),
+            dateval: formattedDate,
+            };
+            return processedSession;
+        });
+        },
+        formattedDate(current) {
+        const options = { month: '2-digit', day: '2-digit', year: 'numeric' };
+        const date = new Date(current);
+        return date.toLocaleDateString('en-US', options);
+        },
+        truncatedEventName(text, maxLength = 22) {
+        if (text && text.length > maxLength) {
+            return text.slice(0, maxLength) + '...';
+        } else {
+            return text;
+        }
+        },
+        truncatedOrgName(text, maxLength = 22) {
+        if (text && text.length > maxLength) {
+            return text.slice(0, maxLength) + '...';
+        } else {
+            return text;
+        }
+        },
+        truncatedHours(text, maxLength = 22) {
+        if (text && text.length > maxLength) {
+            return text.slice(0, maxLength) + '...';
+        } else {
+            return text;
+        }
+        },
+    }
+
+}
 </script>
 
+
+
+
 <style scoped>
+
 .table-wrapper {
-  max-height: 500px;
+  max-height: 400px;
   overflow: auto;
   display:inline-block;
   width: 90%;
@@ -58,4 +122,22 @@
   padding-right: auto
 }
 
+.responsive-text {
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    font-size: 16px;
+}
+
+@media (max-width: 576px) {
+    .responsive-text {
+        font-size: 14px;
+    }
+}
+
+.theadsticky {
+  position: sticky;
+  top: 0;
+  background-color: #e6e7eb !important;
+}
 </style>
